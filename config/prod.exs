67bf5f01a2c87c21d71afae13e853f75ae0bef4d
Alex Shneyderman <a.shneyderman@gmail.com>
2019-09-29 16:42:38 -0400

    Deployment mods

--- config/prod.exs
+++ config/prod.exs
@@ -53,3 +53,8 @@ config :logger, level: :info
 # Finally import the config/prod.secret.exs which loads secrets
 # and configuration from environment variables.
 #import_config "prod.secret.exs"
+config :live_view_demo, LiveViewDemoWeb.Endpoint
+  http: [port: {:system, "PORT"}], # Possibly not needed, but doesn't hurt
+  url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 80],
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
+  server: true
